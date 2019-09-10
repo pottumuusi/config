c1dec0a639b45d79e55112cdbf99dec036242495
Aleksi Backman <bddubbb@gmail.com>
2019-09-10 17:56:37 +0000

    Copy configuration files in NixOS install

--- src/os_install/install_nixos.sh
+++ src/os_install/install_nixos.sh
@@ -7,7 +7,15 @@ volume_group="vg01"
 boot_partition="/dev/nvme0n1p2"
 swap_partition="/dev/nvme0n1p6"
 nixos_root_name="dom0_nixos"
-nixos_saved_config="~/config/useful-files/config/nixos/configuration.nix"
+
+config_repo_top="${HOME}/config/useful-files"
+nixos_saved_config="${config_repo_top}/config/nixos/configuration.nix"
+alsa_config="${config_repo_top}/config/alsa/asound.conf"
+i3_config="${config_repo_top}/config/i3/.config/i3/config"
+i3status_config="${config_repo_top}/config/i3status/.config/i3status/config"
+tmux_config="${config_repo_top}/config/tmux/oh_my_tmux/.tmux.conf"
+tmux_config_local="${config_repo_top}/config/tmux/oh_my_tmux/.tmux.conf.local"
+vimrc="${config_repo_top}/config/vim/.vimrc"
 
 # loadkeys fi
 # nix-env -i wget
@@ -28,4 +36,12 @@ sed -i 's/username/'"$username"'/g' /mnt/etc/nixos/configuration.nix
 
 nixos-install
 
-# TODO copy configuration files
+mkdir -p /mnt/home/${username}/.config/i3
+mkdir -p /mnt/home/${username}/.config/i3status
+
+cp ${alsa_config} /mnt/etc/
+cp ${i3_config} /mnt/home/${username}/.config/i3/
+cp ${i3status_config} /mnt/home/${username}/.config/i3status/
+cp ${tmux_config} /mnt/home/${username}/
+cp ${tmux_config_local} /mnt/home/${username}/
+cp ${vimrc} /mnt/home/${username}/
