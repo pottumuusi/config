13bf3dd99eca318d5c0849cc61e5e62ffe788b4c
Aleksi Backman <bddubbb@gmail.com>
2020-03-20 23:50:14 +0200

    Add script for downloading suckless sources in use

---
+++ src/suckless/dload_sources.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://dl.suckless.org/dwm/dwm-6.2.tar.gz
+wget https://dl.suckless.org/st/st-0.8.2.tar.gz
+wget https://st.suckless.org/patches/solarized/st-solarized-dark-20180411-041912a.diff
