254331c87d5391266ba15a437d8c7c0acbacc259
Aleksi Backman <bddubbb@gmail.com>
2020-10-17 10:17:16 +0300

    Add dwm rotatestack patch to be downloaded

--- src/suckless/dload_sources.sh
+++ src/suckless/dload_sources.sh
@@ -3,3 +3,4 @@
 wget https://dl.suckless.org/dwm/dwm-6.2.tar.gz
 wget https://dl.suckless.org/st/st-0.8.2.tar.gz
 wget https://st.suckless.org/patches/solarized/st-solarized-dark-20180411-041912a.diff
+wget https://dwm.suckless.org/patches/rotatestack/dwm-rotatestack-20161021-ab9571b.diff
