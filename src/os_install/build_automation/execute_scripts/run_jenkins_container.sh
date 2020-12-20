da495c03f480064f6c11848637ff214eac8d71b6
Aleksi Backman <bddubbb@gmail.com>
2020-12-20 12:33:51 +0200

    Remove files moved to bringup repo

--- src/os_install/build_automation/execute_scripts/run_jenkins_container.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p $HOME/my/data/for_services/jenkins
-sudo docker run -p 8080:8080 -p 50000:50000 -v $HOME/my/data/for_services/jenkins:/var/jenkins_home jenkins
--- src/os_install/lvm_clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-lvremove /dev/vg01/root
-lvremove /dev/vg01/home
-vgremove vg01
-pvremove /dev/sda3
