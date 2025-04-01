2aaeb36a88a0b179dad6865bfcffe48aba5b1596
Aleksi Backman <bddubbb@protonmail.com>
2025-04-01 10:51:53 +0300

    Add a script for creating tmux session

---
+++ src/tmux/tmux_new_session.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+main() {
+    local -r session_name='main'
+    local window=''
+
+    tmux new-session -d -s "${session_name}"
+
+    window='0'
+    tmux rename-window -t ${session_name}:${window} 'programs'
+
+    window='1'
+    tmux new-window -t ${session_name}:${window} -n 'maintenance'
+
+    window='2'
+    tmux new-window -t ${session_name}:${window} -n 'rogue-forever'
+
+    window='3'
+    tmux new-window -t ${session_name}:${window} -n 'poet'
+
+    window='4'
+    tmux new-window -t ${session_name}:${window} -n 'c-encapsulation'
+
+    window='5'
+    tmux new-window -t ${session_name}:${window} -n 'c-logging'
+
+    tmux select-window -t ${session_name}:1
+
+    tmux attach-session -t ${session_name}
+}
+
+main "${@}"
